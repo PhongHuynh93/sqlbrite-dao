d23f8f80b455e010363df5e113bb2b5777ca04c5
Phong <p7157568@gmail.com>
2016-09-01 14:11:19 +0700

    feat - create DAO class
     * todo create DAO db
    * todo implement createTable to create table
    * todo create table with 4 column
         * todo must have implement this method, upgrade the db
             * todo query the db to get the cursor
         * todo contentValues is auto create method, use to insert to db

--- sample/src/main/java/com/hannesdorfmann/sqlbritedao/sample/MainActivity.java
+++ sample/src/main/java/com/hannesdorfmann/sqlbritedao/sample/MainActivity.java
@@ -7,13 +7,17 @@ import android.support.v7.widget.RecyclerView;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.hannesdorfmann.sqlbritedao.sample.model.CustomerDao;
+import com.hannesdorfmann.sqlbritedao.sample.model.customer.Customer;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import com.hannesdorfmann.sqlbritedao.sample.model.CustomerDao;
-import com.hannesdorfmann.sqlbritedao.sample.model.customer.Customer;
-import java.util.List;
-import javax.inject.Inject;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
@@ -21,75 +25,89 @@ import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Inject CustomerDao customerDao;
-  @Bind(R.id.recyclerView) RecyclerView recyclerView;
-  @Bind(R.id.cid) EditText idEdit;
-  @Bind(R.id.lastname) EditText lastnameEdit;
-  @Bind(R.id.firstname) EditText firstnameEdit;
-  @Bind(R.id.adult) CheckBox adultCheckBox;
+    @Inject
+    CustomerDao customerDao;
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+    @Bind(R.id.cid)
+    EditText idEdit;
+    @Bind(R.id.lastname)
+    EditText lastnameEdit;
+    @Bind(R.id.firstname)
+    EditText firstnameEdit;
+    @Bind(R.id.adult)
+    CheckBox adultCheckBox;
 
-  CustomerAdapter adapter;
+    CustomerAdapter adapter;
 
-  Subscription customersSubscription;
-  Subscriber<Long> insertSubscription;
+    Subscription customersSubscription;
+    Subscriber<Long> insertSubscription;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ButterKnife.bind(this);
-    injectDependencies();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        injectDependencies();
 
-    adapter = new CustomerAdapter(this);
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    recyclerView.setAdapter(adapter);
+        adapter = new CustomerAdapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
 
-    customersSubscription = customerDao.getCustomers().observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<List<Customer>>() {
-      @Override public void call(List<Customer> customers) {
-        adapter.setCustomers(customers);
-        adapter.notifyDataSetChanged();
-      }
-    }, new Action1<Throwable>() {
-      @Override public void call(Throwable e) {
-        e.printStackTrace();
-        Toast.makeText(MainActivity.this, "Error! check logcat", Toast.LENGTH_SHORT).show();
-      }
-    });
-  }
-
-  @Override protected void onDestroy() {
-    super.onDestroy();
-    customersSubscription.unsubscribe();
-    if (insertSubscription != null) {
-      insertSubscription.unsubscribe();
-    }
-  }
-
-  @OnClick(R.id.insert) public void onAddClicked() {
-    if (insertSubscription != null) {
-      insertSubscription.unsubscribe();
+        customersSubscription = customerDao.getCustomers().observeOn(AndroidSchedulers.mainThread()).subscribe(new Action1<List<Customer>>() {
+            @Override
+            public void call(List<Customer> customers) {
+                adapter.setCustomers(customers);
+                adapter.notifyDataSetChanged();
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Error! check logcat", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    insertSubscription = new Subscriber<Long>() {
-      @Override public void onCompleted() {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        customersSubscription.unsubscribe();
+        if (insertSubscription != null) {
+            insertSubscription.unsubscribe();
+        }
+    }
 
-      }
+    @OnClick(R.id.insert)
+    public void onAddClicked() {
+        if (insertSubscription != null) {
+            insertSubscription.unsubscribe();
+        }
 
-      @Override public void onError(Throwable e) {
-        e.printStackTrace();
-        Toast.makeText(MainActivity.this, "Error! check logcat", Toast.LENGTH_SHORT).show();
-      }
+        insertSubscription = new Subscriber<Long>() {
+            @Override
+            public void onCompleted() {
 
-      @Override public void onNext(Long aLong) {
-        Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_LONG).show();
-      }
-    };
+            }
 
-    customerDao.insert(Integer.parseInt(idEdit.getText().toString()),
-        firstnameEdit.getText().toString(), lastnameEdit.getText().toString(),
-        adultCheckBox.isChecked()).subscribe(insertSubscription);
-  }
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Error! check logcat", Toast.LENGTH_SHORT).show();
+            }
 
-  private void injectDependencies() {
-    ((SampleApp) getApplication()).getObjectGraph().inject(this);
-  }
+            @Override
+            public void onNext(Long aLong) {
+                Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        customerDao.insert(Integer.parseInt(idEdit.getText().toString()),
+                firstnameEdit.getText().toString(), lastnameEdit.getText().toString(),
+                adultCheckBox.isChecked()).subscribe(insertSubscription);
+    }
+
+    private void injectDependencies() {
+        ((SampleApp) getApplication()).getObjectGraph().inject(this);
+    }
 }
--- sample/src/main/java/com/hannesdorfmann/sqlbritedao/sample/model/CustomerDao.java
+++ sample/src/main/java/com/hannesdorfmann/sqlbritedao/sample/model/CustomerDao.java
@@ -2,41 +2,94 @@ package com.hannesdorfmann.sqlbritedao.sample.model;
 
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+
 import com.hannesdorfmann.sqlbrite.dao.Dao;
 import com.hannesdorfmann.sqlbritedao.sample.model.customer.Customer;
 import com.hannesdorfmann.sqlbritedao.sample.model.customer.CustomerMapper;
+
 import java.util.List;
+
 import rx.Observable;
 
 /**
  * @author Hannes Dorfmann
  */
+
+/**
+ * todo create DAO db
+ * define methods to manipulate or query your database table.
+ */
 public class CustomerDao extends Dao {
 
-  @Override public void createTable(SQLiteDatabase database) {
+    /**
+     * todo implement createTable to create table
+     *
+     * @param database
+     */
+    @Override
+    public void createTable(SQLiteDatabase database) {
+        /**
+         * todo create table with 4 column
+         */
+        CREATE_TABLE(
+                Customer.TABLE_NAME,
+                Customer.COL_ID + " INTEGER PRIMARY KEY NOT NULL",
+                Customer.COL_FIRSTNAME + " TEXT",
+                Customer.COL_LASTNAME + " TEXT",
+                Customer.COL_ADULT + " BOOLEAN"
+        ).execute(database);
+    }
 
-    CREATE_TABLE(Customer.TABLE_NAME, Customer.COL_ID + " INTEGER PRIMARY KEY NOT NULL",
-        Customer.COL_FIRSTNAME + " TEXT", Customer.COL_LASTNAME + " TEXT",
-        Customer.COL_ADULT + " BOOLEAN").execute(database);
-  }
+    /**
+     * todo must have implement this method, upgrade the db
+     *
+     * @param db         the database
+     * @param oldVersion old database version
+     * @param newVersion new database version
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-  @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
 
-  }
+    public Observable<List<Customer>> getCustomers() {
+        /**
+         * todo query the db to get the cursor
+         * translate cursor by mapToList (CustomerMapper.MAPPER is auto created)
+         */
+        return query(
+                SELECT(
+                        Customer.COL_ID,
+                        Customer.COL_FIRSTNAME,
+                        Customer.COL_LASTNAME,
+                        Customer.COL_ADULT
+                ).FROM(Customer.TABLE_NAME))
+                .run()
+                .mapToList(CustomerMapper.MAPPER);
+    }
 
-  public Observable<List<Customer>> getCustomers() {
-    return query(SELECT(Customer.COL_ID, Customer.COL_FIRSTNAME, Customer.COL_LASTNAME,
-        Customer.COL_ADULT).FROM(Customer.TABLE_NAME)).run().mapToList(CustomerMapper.MAPPER);
-  }
+    /**
+     * build a content provider to insert to db.
+     * Default if we dont use sqlbrite dao, we can have declare constant which column is add.
+     *
+     * but simpler if we use sqlbrite dao, so the field in class is the column
+     * {@link Customer}
+     * todo contentValues is auto create method
+     * @param id
+     * @param firstname
+     * @param lastname
+     * @param adult
+     *
+     * @return
+     */
+    public Observable<Long> insert(int id, String firstname, String lastname, boolean adult) {
+        ContentValues values = CustomerMapper.contentValues()
+                .id(id)
+                .firstname(firstname)
+                .lastname(lastname)
+                .adult(adult)
+                .build();
 
-  public Observable<Long> insert(int id, String firstname, String lastname, boolean adult) {
-    ContentValues values = CustomerMapper.contentValues()
-        .id(id)
-        .firstname(firstname)
-        .lastname(lastname)
-        .adult(adult)
-        .build();
-
-    return insert(Customer.TABLE_NAME, values);
-  }
+        return insert(Customer.TABLE_NAME, values);
+    }
 }
